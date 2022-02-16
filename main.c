6c794dee1218ccb44c7aa7f51a0fa2cf4c47bb58
Sparpvp <sparpi06@gmail.com>
2022-02-16 02:06:58 +0100

    Abstracted! Now you can use this as a lib!

--- main.c
+++ main.c
@@ -2,149 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include <curl/curl.h>
+#include "wrap.h"
 
-void SkipPeerVerification(CURL *curl) 
+int main(int argc, char *argv[])
 {
-    // Connect to a site that isn't using a signed certificate.
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-    printf("Peer Verification Skipped\n");
-}
+    PrintUsage(argc, argv[0], argv[1]);
 
-void printHelp(char *argv[]) {
-    printf("Usage: %s [options...] <url> \n-g, --get\tExecute HTTP GET Request\n-p, --post \"<data>\"\tExecute HTTP POST Request\n[Optional] -s, --skip\tSkip Peer Verification (skip signed SSL certificate)\n\nExample: %s -g https://api.ipify.org\n", argv[0], argv[0]);
-}
+    Request request;
+    Init(&request);
 
-int main(int argc, char *argv[]) 
-{
-    char a[7];
-    if(argc == 1)
-    {
-        printf("%s: Missing arguments... try '%s --help' for more information", argv[0], argv[0]);
-        exit(EXIT_FAILURE);
-    }
-    if(strcmp(argv[1], "-h") == 0) {
-        printHelp(&argv[0]);
-        exit(EXIT_SUCCESS);
-    }
-    if(strcmp(argv[1], "--help") == 0) 
-    {
-        printHelp(&argv[0]);
-        exit(EXIT_SUCCESS);
-    }
+    CheckRequestType(&request, argv[1]);
 
-    CURL *curl = curl_easy_init();
-    CURLcode res;
-    curl_global_init(CURL_GLOBAL_DEFAULT);
-    curl = curl_easy_init();
-    bool skiparg = false;
+    PeerSSLVerify(&request, argv[3], argv[4]);
 
-    
-    if(argv[3] == NULL) 
-    {
-        skiparg = true;
-        goto skip;
-    }
-    if(argv[4] == NULL && (0 == strcmp(argv[1], "-p")))
-    {
-        skiparg = true;
-        goto skip;
-    }
-    if(0 == strcmp(argv[3], "-s")) {
-        SkipPeerVerification(curl);
-    }
-    else if(0 == strcmp(argv[3], "--skip")) 
-    {
-        SkipPeerVerification(curl);
-    }
-    else if(0 == strcmp(argv[4], "-s")) {
-        SkipPeerVerification(curl);
-    }
-    else if(0 == strcmp(argv[4], "--skip")) 
-    {
-        SkipPeerVerification(curl);
-    }
-    skip:
-    if(skiparg)
-    {
-        printf("Peer Verification Not Skipped\n");
-    }
-    if(0 == strcmp(argv[1], "-g")) 
-    {
-        if(curl)
-        {
-            curl_easy_setopt(curl, CURLOPT_URL, argv[2]);
-            printf("Result: ");
-            // Perform request; res gets return code
-            res = curl_easy_perform(curl);
-            if(res != CURLE_OK) 
-            {
-                fprintf(stderr, "Failed request performing.\n");
-                // Always cleanup
-                curl_easy_cleanup(curl);
-            }
-            // Fish shell doesn't write in newline automatically
-            printf("\n");
-        }
-    }
-    if(0 == strcmp(argv[1], "--get")) 
-    {
-        if(curl) 
-        {
-            curl_easy_setopt(curl, CURLOPT_URL, argv[2]);
-            printf("Result: ");
-            // Perform request; res gets return code
-            res = curl_easy_perform(curl);
-            if(res != CURLE_OK) 
-            {
-                fprintf(stderr, "Failed request performing.\n");
-                // Always cleanup
-                curl_easy_cleanup(curl);
-            }
-            // Fish shell doesn't write in newline automatically
-            printf("\n");
-        }
-    }
-    if(0 == strcmp(argv[1], "-p")) 
-    {
-        if(curl) 
-        {
-            // Specify Link for the POST request
-            curl_easy_setopt(curl, CURLOPT_URL, argv[3]);
-            
-            // Specify POST data
-            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, argv[2]);
-        
-            // Perform the request, res will get the return code
-            res = curl_easy_perform(curl);
-            
-            if(res != CURLE_OK)
-                fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+    // HTTP/GET
+    if (request.rt == GET)
+        if (request.curl)
+            SendGET(&request, argv[2]);
 
-            curl_easy_cleanup(curl);    
-        }
-    }
-    if(0 == strcmp(argv[1], "--post")) 
-    {
-        if(curl) 
-        {
-            // Specify Link for the POST request
-            curl_easy_setopt(curl, CURLOPT_URL, argv[3]);
-            
-            // Specify POST data
-            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, argv[2]);
-        
-            // Perform the request, res will get the return code
-            res = curl_easy_perform(curl);
-            
-            if(res != CURLE_OK)
-                fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-
-            curl_easy_cleanup(curl);    
-        }
-    }
-
-    curl_global_cleanup();
+    // HTTP/POST
+    if (request.rt == POST)
+        if (request.curl)
+            SendPOST(&request, argv[3], argv[2]);
 
     return 0;
 }
---
+++ wrap.h
@@ -0,0 +1,109 @@
+#pragma once
+#include <curl/curl.h>
+
+enum RequestType
+{
+    GET,
+    POST
+};
+typedef enum RequestType RequestType;
+
+struct Request
+{
+    CURL *curl;
+    CURLcode res;
+    bool skiparg;
+    RequestType rt;
+};
+typedef struct Request Request;
+
+void CheckRequestType(Request *r, char *arg)
+{
+    if (0 == strcmp(arg, "-p") || 0 == strcmp(arg, "--post"))
+        r->rt = POST;
+    else
+        r->rt = GET;
+}
+
+void PrintMissingArguments(char *arg)
+{
+    printf("%s: Missing arguments... try '%s --help' for more information\n", arg, arg);
+    exit(EXIT_FAILURE);
+}
+
+void PrintHelp(char *arg)
+{
+    printf("Usage: %s [Required Options] <url> [Optional Settings...] \n-g, --get\t\tSend HTTP GET Request\n-p, --post \"<data>\"\tSend HTTP POST Request\n[Optional] -s, --skip\tSkip Peer Verification (skip signed SSL certificate verify)\n\nExample: %s -g https://api.ipify.org\n", arg, arg);
+    exit(EXIT_SUCCESS);
+}
+
+void PrintUsage(int argc, char *arg0, char *arg1)
+{
+    if (argc == 1)
+        PrintMissingArguments(arg0);
+    if (strcmp(arg1, "-h") == 0 || strcmp(arg1, "--help") == 0)
+        PrintHelp(arg1);
+}
+
+void PeerSSLVerify(Request *r, char *arg3, char *arg4)
+{
+    if (r->rt == GET && arg3 == NULL || r->rt == POST && arg4 == NULL)
+    {
+        printf("SSL Peer certification verify not skipped\n");
+        return;
+    }
+
+    // Connect to a site that isn't using a signed certificate.
+    if (r->rt == GET && 0 == strcmp(arg3, "-s"))
+        curl_easy_setopt(r->curl, CURLOPT_SSL_VERIFYPEER, 0L);
+    else if (r->rt == GET && 0 == strcmp(arg3, "--skip"))
+        curl_easy_setopt(r->curl, CURLOPT_SSL_VERIFYPEER, 0L);
+    else if (r->rt == POST && 0 == strcmp(arg4, "-s"))
+        curl_easy_setopt(r->curl, CURLOPT_SSL_VERIFYPEER, 0L);
+    else if (r->rt == POST && 0 == strcmp(arg4, "--skip"))
+        curl_easy_setopt(r->curl, CURLOPT_SSL_VERIFYPEER, 0L);
+
+    printf("SSL Peer certification verify skipped\n");
+}
+
+void Init(Request *r)
+{
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    r->curl = curl_easy_init();
+    r->skiparg = false;
+}
+
+void SendGET(Request *r, char *url)
+{
+    curl_easy_setopt(r->curl, CURLOPT_URL, url);
+    printf("Response: ");
+    // Perform request; res gets return code
+    r->res = curl_easy_perform(r->curl);
+    if (r->res != CURLE_OK)
+    {
+        fprintf(stderr, "Failed request performing.\n");
+        // Always cleanup
+        curl_easy_cleanup(r->curl);
+    }
+    // Fish shell (and some others) doesn't write in newline automatically
+    printf("\n");
+}
+
+void SendPOST(Request *r, char *url, char *postData)
+{
+    // Specify Link for the POST request
+    curl_easy_setopt(r->curl, CURLOPT_URL, url);
+
+    // Specify POST data
+    curl_easy_setopt(r->curl, CURLOPT_POSTFIELDS, postData);
+
+    // Perform the request, res will get the return code
+    r->res = curl_easy_perform(r->curl);
+
+    if (r->res != CURLE_OK)
+        fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(r->res));
+
+    curl_easy_cleanup(r->curl);
+
+    printf("Request sent!\n");
+}
